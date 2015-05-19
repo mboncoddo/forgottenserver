b55a8a2c8965964c0b483f4b89e28703dde48d52
Ranieri Althoff <ranisalt@gmail.com>
2015-05-19 13:14:46 -0300

    Update database, create migration and bump version

--- data/migrations/18.lua
+++ data/migrations/18.lua
@@ -1,3 +1,5 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 19 (alter password field to handle different encryption)")
+	db.query("ALTER TABLE `accounts` CHANGE `password` `password` VARCHAR(128) NOT NULL")
+	return true
 end
